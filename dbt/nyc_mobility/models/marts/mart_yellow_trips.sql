c8293991debca4983df36619e33c10204252f410
Anailson Rayran <anailsonrayran921@icloud.com>
2026-09-22 21:49:25 -0300

    feat: add Power BI dashboard and update dbt mart

--- dbt/nyc_mobility/models/marts/mart_yellow_trips.sql
+++ dbt/nyc_mobility/models/marts/mart_yellow_trips.sql
@@ -12,11 +12,23 @@ select
     vendor_id,
     tpep_pickup_datetime,
     tpep_dropoff_datetime,
+    extract(hour from tpep_pickup_datetime) as pickup_hour,
     passenger_count,
     trip_distance,
     pu_location_id,
     do_location_id,
     payment_type,
+
+    case
+        when payment_type = 1 then 'Credit Card'
+        when payment_type = 2 then 'Cash'
+        when payment_type = 3 then 'No Charge'
+        when payment_type = 4 then 'Dispute'
+        when payment_type = 5 then 'Unknown'
+        when payment_type = 6 then 'Voided Trip'
+        else 'Other'
+    end as payment_type_name,
+
     fare_amount,
     tip_amount,
     total_amount,
